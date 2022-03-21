8e976dc06b61dd6a0fc435feaec08825ad28321a
akio <akioUnity@outlook.com>
2022-03-21 20:22:12 +0700

    fix whatsapp price issue

--- build_whatsapp_code.php
+++ build_whatsapp_code.php
@@ -5,8 +5,8 @@ $whatsapp_code = "<h4>per Whats APP : "
 if ($user_ID == 0)
     $whatsapp_code .= " class='button_disabled'";
 else
-    $whatsapp_code .= "class='whatsapp-button' onclick=\"call_whatsapp(" . $agentresult->ID . ",'" . $agentresult->bezeichnung . "','" . $agentresult->sbid . "','" . $agentresult->mobilenumber_1 . "'," . $agentresult->chatpreis_9 . ")\" ";
-$whatsapp_code .= "width=\"30\"> ab ".$agentresult->chatpreis_9."  €</h4> <p class='whatsapp_message'></p>";
+    $whatsapp_code .= "class='whatsapp-button' onclick=\"call_whatsapp(" . $agentresult->ID . ",'" . $agentresult->bezeichnung . "','" . $agentresult->sbid . "','" . $agentresult->mobilenumber_1 . "'," . $agentresult->chatpreis_1 . ")\" ";
+$whatsapp_code .= "width=\"30\"> ab ".$agentresult->chatpreis_1."  €</h4> <p class='whatsapp_message'></p>";
 
 // "wp-content/plugins/whatsapp-payment/";--
 //
